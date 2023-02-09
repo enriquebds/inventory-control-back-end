0b4c72b2386a5f19d94c4e34c537673aad21dfe0
Enrique <enrique.barbosasilva@gmail.com>
2023-02-09 18:23:25 -0300

    feat: manager-routes

--- src/routes/manager.routes.ts
+++ src/routes/manager.routes.ts
@@ -1,1 +1,14 @@
 import { Router } from "express";
+import { ManagerControllers } from "../controllers/Manager/manager.controller";
+
+const managerControllers = new ManagerControllers();
+
+const route = Router();
+
+route.post("", managerControllers.create);
+route.get("", managerControllers.list);
+route.get("/:id", managerControllers.listById);
+route.delete("/:id", managerControllers.delete);
+route.patch("/:id", managerControllers.update);
+
+export default route;
