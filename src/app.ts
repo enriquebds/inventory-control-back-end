c49c5a09e86814711ae55d8e2baf89f0890dc254
Enrique <enrique.barbosasilva@gmail.com>
2023-02-09 18:20:53 -0300

    refactor: manager-routes-added-in-app

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import { handleErrors } from "./errors/handleError";
 import "express-async-errors";
+import managerRoutes from "../src/routes/manager.routes";
 
 let cors = require("cors");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/manager", managerRoutes);
 app.use(handleErrors);
 
 export default app;
