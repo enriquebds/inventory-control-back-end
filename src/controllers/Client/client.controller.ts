d67803be36402ca67169650bbee86a3f14969530
Enrique <enrique.barbosasilva@gmail.com>
2023-02-09 20:17:21 -0300

    feat: client-controllers

--- src/controllers/Client/client.controller.ts
+++ src/controllers/Client/client.controller.ts
@@ -1,1 +1,54 @@
 import { Request, Response } from "express";
+import createClientService from "../../services/Client/createClient.service";
+import deleteClientService from "../../services/Client/deleteClient.service";
+import listClientService from "../../services/Client/listClient.service";
+import listClientById from "../../services/Client/listClientById.service";
+import updateClientService from "../../services/Client/updateClient.service";
+
+export class ClientControllers {
+  async create(req: Request, res: Response) {
+    const { name, email, password } = req.body;
+
+    const client = await createClientService({ name, email, password });
+
+    return res.status(201).json(client);
+  }
+
+  async list(req: Request, res: Response) {
+    const clients = await listClientService();
+
+    return res.json(clients);
+  }
+
+  async listById(req: Request, res: Response) {
+    const { id } = req.params;
+
+    const client = await listClientById(id);
+
+    return res.json(client);
+  }
+
+  async delete(req: Request, res: Response) {
+    const { id } = req.params;
+
+    await deleteClientService(id);
+
+    return res.json({
+      message: "Client deleted",
+    });
+  }
+
+  async patch(req: Request, res: Response) {
+    const { id } = req.params;
+    const { name, email, password } = req.body;
+
+    const updatedClient = await updateClientService({
+      id,
+      name,
+      email,
+      password,
+    });
+
+    return res.json(updatedClient);
+  }
+}
