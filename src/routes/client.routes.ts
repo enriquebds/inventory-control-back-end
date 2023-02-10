355b8abb0153849e525bed8a0576f48f480b3983
Enrique <enrique.barbosasilva@gmail.com>
2023-02-10 15:59:21 -0300

    refactor: client-routes-changed

--- src/routes/client.routes.ts
+++ src/routes/client.routes.ts
@@ -1,14 +1,20 @@
 import { Router } from "express";
 import { ClientControllers } from "../controllers/Client/client.controller";
+import tokenAuthMiddleware from "../middlewares/tokenAuth.middleware";
+import { verifyManager } from "../middlewares/verifyIsManager.middleware";
 
 const clientControllers = new ClientControllers();
 const route = Router();
 
 route.post("", clientControllers.create);
-route.post("/product/:id/:idUser", clientControllers.addingProduct);
-route.get("", clientControllers.list);
+route.post(
+  "/product/:id/:idUser",
+  tokenAuthMiddleware,
+  clientControllers.addingProduct
+);
+route.get("", tokenAuthMiddleware, verifyManager, clientControllers.list);
 route.get("/:id", clientControllers.listById);
-route.delete("/:id", clientControllers.delete);
-route.patch("/:id", clientControllers.patch);
+route.delete("/:id", tokenAuthMiddleware, clientControllers.delete);
+route.patch("/:id", tokenAuthMiddleware, clientControllers.patch);
 
 export default route;
