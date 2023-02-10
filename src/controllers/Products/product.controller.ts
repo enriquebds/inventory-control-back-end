2adbb2a3679690ee41e40d3b9a9f464aa1aaf653
Enrique <enrique.barbosasilva@gmail.com>
2023-02-10 11:47:58 -0300

    feat: product-controllers

--- src/controllers/Products/product.controller.ts
+++ src/controllers/Products/product.controller.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from "express";
 import createProductService from "../../services/Product/createProduct.service";
+import deleteProductService from "../../services/Product/deleteProduct.service";
 import listProductService from "../../services/Product/listProduct.service";
+import listProductByIdService from "../../services/Product/listProductById.service";
+import updateProductService from "../../services/Product/updateProduct.service";
 
 export class ProductControllers {
   async create(req: Request, res: Response) {
@@ -21,5 +24,37 @@ export class ProductControllers {
     const products = await listProductService();
 
     return res.json(products);
+  }
+
+  async listById(req: Request, res: Response) {
+    const { id } = req.params;
+
+    const product = await listProductByIdService(id);
+
+    return res.json(product);
+  }
+
+  async delete(req: Request, res: Response) {
+    const { id } = req.params;
+
+    await deleteProductService(id);
+
+    return res.json({
+      message: "Product deleted",
+    });
+  }
+
+  async update(req: Request, res: Response) {
+    const { id } = req.params;
+    const { name, category, description } = req.body;
+
+    const updatedProduct = await updateProductService({
+      id,
+      name,
+      category,
+      description,
+    });
+
+    return res.json(updatedProduct);
   }
 }
