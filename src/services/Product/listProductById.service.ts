0d9ac6c5fbe22894e36c7aa1558215df2b9e2727
Enrique <enrique.barbosasilva@gmail.com>
2023-02-10 11:46:39 -0300

    feat: listProductById-service

---
+++ src/services/Product/listProductById.service.ts
@@ -0,0 +1,33 @@
+import { prisma } from "../../../prisma/client/client";
+import { AppError } from "../../errors/AppError";
+import { IProductRequest } from "../../interfaces/product";
+
+const listProductByIdService = async (id: string): Promise<IProductRequest> => {
+  const product = await prisma.product.findFirst({
+    where: {
+      id,
+    },
+    select: {
+      id: true,
+      name: true,
+      category: true,
+      description: true,
+      manager: {
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          isManager: true,
+        },
+      },
+    },
+  });
+
+  if (!product) {
+    throw new AppError(404, "Product not found");
+  }
+
+  return product;
+};
+
+export default listProductByIdService;
