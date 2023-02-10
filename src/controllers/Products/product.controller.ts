5033b4c57863e65a91e4da3e41979bfc954e8cdc
Enrique <enrique.barbosasilva@gmail.com>
2023-02-09 23:16:01 -0300

    feat: product-controllers

--- src/controllers/Products/product.controller.ts
+++ src/controllers/Products/product.controller.ts
@@ -1,1 +1,25 @@
 import { Request, Response } from "express";
+import createProductService from "../../services/Product/createProduct.service";
+import listProductService from "../../services/Product/listProduct.service";
+
+export class ProductControllers {
+  async create(req: Request, res: Response) {
+    const { id } = req.params;
+    const { name, category, description } = req.body;
+
+    const product = await createProductService({
+      id,
+      name,
+      category,
+      description,
+    });
+
+    return res.status(201).json(product);
+  }
+
+  async list(req: Request, res: Response) {
+    const products = await listProductService();
+
+    return res.json(products);
+  }
+}
