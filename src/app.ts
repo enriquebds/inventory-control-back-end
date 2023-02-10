d1a265ab527947c6cfe0577c4857dedcaa608c71
Enrique <enrique.barbosasilva@gmail.com>
2023-02-09 23:09:24 -0300

    refactor: product-routes

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,14 @@ import { handleErrors } from "./errors/handleError";
 import "express-async-errors";
 import managerRoutes from "../src/routes/manager.routes";
 import clientRoutes from "../src/routes/client.routes";
+import productRoutes from "../src/routes/product.routes";
 
 let cors = require("cors");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/product", productRoutes);
 app.use("/client", clientRoutes);
 app.use("/manager", managerRoutes);
 app.use(handleErrors);
